44752380e562d843a8126075a1e5c98a2d9f15a9
azrias789 <148734755+azrias789@users.noreply.github.com>
2024-01-01 21:35:37 +0800

    testcal.py

--- testcal.py
+++ testcal.py
@@ -1,10 +1,17 @@
 import unittest
+from cal.py import *
 
-class testcal(unittest.TestCase):
-  def test_msg(self):
-    a = 'some'
-    b = 'some'
-    self.assertEqual(a,b)
+def is_int(val):
+  if type(val) == int:
+      return True
+  else :
+      return False
+
+class TestStringMethods(unittest.TestCase):
+  def test_negative(self):
+    testvalue = False
+    message = "test value is not true"
+    self.assertTrue( testvalue, message)
 
 if __name__ == '__main__':
   unittest.main()
